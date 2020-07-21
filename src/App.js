e2254e08955849b68360c570f36d670d3fc20f91
jigisha <jigisha.rojasara@sublimedatasys.com>
2020-07-21 10:02:34 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+var Tesseract = window.Tesseract;
+
+const App = () => {
+  const [uploads, setUploads] = useState([])
+  const [patterns, setPatterns] = useState([])
+  const [documents, setDocuments] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <h1>My OCR App</h1>
       </header>
+
+      { /* File uploader */}
+      <section className="hero">
+        <label className="fileUploaderContainer">
+          Click here to upload documents
+            <input type="file" id="fileUploader" multiple />
+        </label>
+
+        <div>
+          { /* Previews will be shown here */}
+        </div>
+
+        <button className="button">Generate</button>
+      </section>
+
+      { /* Results */}
+      <section className="results">
+
+        <div className="results__result">
+          <div className="results__result__image">
+            <img width="250px" />
+          </div>
+          <div className="results__result__info">
+            <div className="results__result__info__codes">
+              <small>{ /* Confidence score */}</small>
+            </div>
+            <div className="results__result__info__codes">
+              <small>{ /* Pattern output */}</small>
+            </div>
+            <div className="results__result__info__text">
+              <small>{ /* Full output */}</small>
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        <div className="results__result">
+          { /* Additional output if more than one document is processed */}
+        </div>
+
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
